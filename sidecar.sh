687cafeb596f1d6f164c372824c0895055a0b4dc
Randall Coding <randallcoding@protonmail.com>
2023-12-01 13:28:18 +0700

    Fixes volume mount and sidecar

--- sidecar.sh
+++ sidecar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Running volume permissions sidecar.sh"
 adduser -uid 8983 --disabled-password --gecos "" solr 
-chown -R 8983:8983 /prometheus
+chown -R 8983:8983 /var/solr/
 echo "Permissions changed"
 exit 0
